871a82aed94e79a232dd7a9d6e6930927d64d416
Roberta <bebetinha.joaomiguel@gmail.com>
2025-08-26 00:03:07 -0300

    Update 01_create_tables_bigquery.sql
     criacao das tabelas .

--- 01_create_tables_bigquery.sql
+++ 01_create_tables_bigquery.sql
@@ -1,1 +1,10 @@
+-- CLIENTES:
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Clientes` (ID_Cliente INT64, Nome_Cliente STRING, Email_Cliente STRING, Estado_Cliente STRING);
+
+-- PRODUTOS:
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` (ID_Produto INT64 , Nome_Produto STRING , Categoria_Produto STRING , Preco_Produto NUMERIC );
+
+-- VENDAS: 
+CREATE OR REPLACE TABLE `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Vendas` (ID_Venda INT64 , ID_Cliente INT64 , ID_Produto INT64 , Data_Venda DATE , Quantidade INT64);
+
 
