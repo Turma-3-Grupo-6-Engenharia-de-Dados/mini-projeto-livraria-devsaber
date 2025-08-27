49b93bd470d5c846c8b4e121fe305cb74b4bdb9d
Caroline Maglio <129905279+carolmaglio@users.noreply.github.com>
2025-08-27 20:54:54 -0300

    02_insert_data_bigquery.sql

--- 02_insert_data_bigquery.sql
+++ 02_insert_data_bigquery.sql
@@ -1,3 +1,4 @@
+-- Clientes
 INSERT INTO `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Clientes` (
   ID_Cliente,
   Nome_Cliente,
@@ -11,19 +12,7 @@ VALUES
   (3, 'Carla Dias', 'carla.d@email.com', 'SP'),
   (4, 'Daniel Souza', 'daniel.s@email.com', 'MG');
 
-INSERT INTO `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` (
-  ID_Produto,
-  Nome_Produto,
-  Categoria_Produto,
-  Preco_Produto
-)
-
-VALUES 
-  (101, 'Fundamentos de SQL', 'Dados', 60.00),
-  (102, 'Duna', 'Ficção Científica', 80.50),
-  (103, 'Python para Dados', 'Programação', 75.00),
-  (104, 'o Guia do Mochileiro', 'Ficção Científica', 42.00);
-
+-- Produtos
 INSERT INTO `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` (
   ID_Produto,
   Nome_Produto,
