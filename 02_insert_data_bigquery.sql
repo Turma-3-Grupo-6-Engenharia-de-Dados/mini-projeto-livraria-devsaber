d53e8863a9f3f75c3bf71d80340c2b755de10063
Caroline Maglio <129905279+carolmaglio@users.noreply.github.com>
2025-08-27 17:34:37 -0300

    02_insert_data_bigquery.sql

--- 02_insert_data_bigquery.sql
+++ 02_insert_data_bigquery.sql
@@ -23,3 +23,20 @@ VALUES
   (102, 'Duna', 'Ficção Científica', 80.50),
   (103, 'Python para Dados', 'Programação', 75.00),
   (104, 'o Guia do Mochileiro', 'Ficção Científica', 42.00);
+
+INSERT INTO `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` (
+  ID_Produto,
+  Nome_Produto,
+  Categoria_Produto,
+  Preco_Produto
+)
+
+VALUES 
+  (101, 'Fundamentos de SQL', 'Dados', 60.00),
+  (102, 'Duna', 'Ficção Científica', 80.50),
+  (103, 'Python para Dados', 'Programação', 75.00),
+  (104, 'o Guia do Mochileiro', 'Ficção Científica', 42.00);
+
+
+
+
