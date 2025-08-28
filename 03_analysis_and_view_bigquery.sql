c4bf8ce4b2febb0cafc99892cbe612a3316c0472
Nathalia Kopke Palma <nkpalma@unifesp.br>
2025-08-28 19:35:32 -0300

    Update 03_analysis_and_view_bigquery.sql

--- 03_analysis_and_view_bigquery.sql
+++ 03_analysis_and_view_bigquery.sql
@@ -39,4 +39,31 @@ GROUP BY P.Nome_Produto
 ORDER BY Total_Quantidade_Vendida DESC
 LIMIT 1;
 
+
 -- CRIAÇÃO DA VIEW --
+CREATE OR REPLACE VIEW `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.v_relatorio_vendas_detalhado` AS
+SELECT
+    V.ID_Venda,
+    V.Data_Venda,
+    C.Nome_Cliente,
+    P.Nome_Produto,
+    V.Quantidade_Venda,
+    (V.Quantidade_Venda * P.Preco_Produto) AS Valor_Total
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Vendas` AS V
+JOIN `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
+JOIN `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
+
+-- Consultar o valor total de cada venda feita para a cliente Ana Silva
+SELECT *
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.v_relatorio_vendas_detalhado` 
+WHERE Nome_Cliente = 'Ana Silva';
+
+-- Consultar o valor total de cada venda feita para a cliente Carla Dias
+SELECT *
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.v_relatorio_vendas_detalhado` 
+WHERE Nome_Cliente = 'Carla Dias';
+
+-- Consultar o valor total de cada venda feita no dia 02/02/2024
+SELECT *
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.v_relatorio_vendas_detalhado` 
+WHERE Data_Venda = '2024-02-02';
