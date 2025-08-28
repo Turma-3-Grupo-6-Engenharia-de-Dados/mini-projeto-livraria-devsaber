c327baca7251bcb98232f39c695b49d41994453b
GiselyKCosta <167264229+GiselyKCosta@users.noreply.github.com>
2025-08-28 18:16:37 -0300

    Update 03_analysis_and_view_bigquery.sql

--- 03_analysis_and_view_bigquery.sql
+++ 03_analysis_and_view_bigquery.sql
@@ -3,13 +3,13 @@
 
 -- Pergunta 1: clientes do estado 'SP'
 SELECT  Nome_Cliente
-FROM  Clientes
+FROM  `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Clientes`
 WHERE  Estado_Cliente  =  'SP';
 
 -- Pergunta 2: produtos de Ficção Científica
 
 SELECT  Nome_Produto
-FROM  Produtos
+FROM  `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos`
 WHERE  Categoria_Produto  =  'Ficção Científica';
 
 -- Pergunta 3: todas as vendas (cliente, produto, data)
@@ -17,24 +17,24 @@ SELECT
   C.Nome_Cliente,
   P.Nome_Produto,
   V.Data_Venda
-FROM `meu_dataset.Vendas` V
-JOIN `meu_dataset.Clientes` C ON V.ID_Cliente = C.ID_Cliente
-JOIN `meu_dataset.Produtos` P ON V.ID_Produto = P.ID_Produto
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Vendas` V
+JOIN `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Clientes` C ON V.ID_Cliente = C.ID_Cliente
+JOIN  `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` P ON V.ID_Produto = P.ID_Produto
 ORDER BY V.Data_Venda;
 
 -- Pergunta 4: valor total de cada venda
 SELECT
   V.ID_Venda,
   V.Quantidade * P.Preco_Produto AS Valor_Total
-FROM `meu_dataset.Vendas` V
-JOIN `meu_dataset.Produtos` P ON V.ID_Produto = P.ID_Produto;
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Vendas` V
+JOIN `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` P ON V.ID_Produto = P.ID_Produto;
 
 -- Pergunta 5: produto mais vendido
 SELECT
   P.Nome_Produto,
   SUM(V.Quantidade) AS Total_Quantidade_Vendida
-FROM `meu_dataset.Vendas` V
-JOIN `meu_dataset.Produtos` P ON V.ID_Produto = P.ID_Produto
+FROM `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Vendas` V
+JOIN `t1engenhariadados.LivrariaDevSaber_Grupo_3_6.Produtos` P ON V.ID_Produto = P.ID_Produto
 GROUP BY P.Nome_Produto
 ORDER BY Total_Quantidade_Vendida DESC
 LIMIT 1;
